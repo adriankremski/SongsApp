5fa7cfd8f0dca4972360d0b7259130c0d69c7078
snuffix <adrian.kremski@gmail.com>
2019-05-23 17:42:12 +0200

    Song model added to domain module

---
+++ domain/src/main/java/com/github/snuffix/songapp/domain/model/Song.kt
@@ -0,0 +1,8 @@
+package com.github.snuffix.songapp.domain.model
+
+import java.util.*
+
+class Song(val id: String,
+           val trackName: String,
+           val releaseDate: Date,
+           val artistName: String)
